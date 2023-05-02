43171189fcb97480ff5feb1c6747879192f29f67
고영준 <>
2023-05-02 19:33:15 +0900

    gugudan youngjun

--- campus-exam-project/src/exam1.java
+++ campus-exam-project/src/exam1.java
@@ -6,34 +6,24 @@ public class exam1 {
 		
 		System.out.println("1 ~ 999사이의 숫자를 입력하시오");
 		Scanner in = new Scanner(System.in);
-		int i =in.nextInt();
+		int i =in.nextInt(); // 입력값을 변수에 넣음 
 		
 		while (true) {
-			
-			i = in.nextInt();
-			
-			try {
 				
-				if(i > 999) {
-					System.out.println("1 ~ 999사이의 숫자를 입력하시오");
-				}
-				else if(i < 1) {
-					System.out.println("1 ~ 999사이의 숫자를 입력하시오");
-				}
-				else {
-					break;
-				}
-
+			if(i > 999 || i < 1) { // 1 ~999 
+				System.out.println("1 ~ 999사이의 숫자를 입력하시오");
+				i = in.nextInt();
 			}
-
-			catch (Exception e) {
-				System.out.println("정수만 입력하세요");
+			else {
+				// System.out.println("정수를 입력하시오"); 
+				break;
 			}
+			
 
 		}
 
 		for (int j = 1; j < 10; j++) {
-			System.out.println(i + "*" + j + "=" + i * j);
+			System.out.println(i + "X" + j + "=" + i * j);
 		}
 	}	
 }
