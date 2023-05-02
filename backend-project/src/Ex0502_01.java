bd38c1be9ced6cb3fb4c1e92148f42d008bdba83
magpie2286 <hunminy1@naver.com>
2023-05-02 18:04:32 +0900

    gugudan

---
+++ backend-project/src/Ex0502_01.java
@@ -0,0 +1,14 @@
+
+public class Ex0502_01 {
+
+	public static void main(String[] args) {
+		
+		for(int i = 2; i < 10; i++) {
+			for(int j = 1; j < 10; j++) {
+				System.out.println(i + "x" + j + "=" + i*j);
+			}
+			System.out.println();
+		}
+	}
+}
+
