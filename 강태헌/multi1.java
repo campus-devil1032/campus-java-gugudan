9dbc91d6728fcbaf97bb3a912313e9a906754e92
teh4 <139567.naver.com>
2023-05-02 16:55:32 +0900

    gugu

---
+++ 강태헌/multi1.java
@@ -0,0 +1,16 @@
+
+public class multi1 {
+	public static void main(String[] args) {
+
+		for (int i = 1; i <= 9; i++) {
+			System.out.println(i + "단");
+			for (int j = 1; j <= 9; j++) {
+				System.out.println(i + "*" + j + "=" + i * j);
+
+			}
+			System.out.println();
+		}
+
+	}
+
+}
