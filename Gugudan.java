f1f132efcf89969da245519b5ab98e84603502d8
wlsrn99 <wlsrn331@naver.com>
2023-05-04 01:18:35 +0900

    update

---
+++ Gugudan.java
@@ -0,0 +1,37 @@
+package gugudan;
+
+import java.util.Scanner;
+
+class Calculator { // 구구단 기능 메인에서 분리
+	public void Calculator(int dan) {
+
+		for (int i = 1; i <= 9; i++) {
+			System.out.println(dan + " X " + i + " = " + (dan * i));
+		}
+	}
+}
+
+public class Gugudan {
+
+	public static boolean MyException(int dan) {
+		if (dan <= 0 || dan > 999) {
+			return false;
+		} else
+			return true;
+	}
+
+	public static void main(String[] args) {
+		int dan = 0;
+		System.out.println("단 수를 입력하세요");
+		Scanner in = new Scanner(System.in);
+		dan = in.nextInt();
+		in.close();
+
+		if (MyException(dan) == false) {
+			System.out.println("예외 조건입니다");
+		} else {
+			Calculator gu = new Calculator();
+			gu.Calculator(dan);
+		}
+	}
+}
