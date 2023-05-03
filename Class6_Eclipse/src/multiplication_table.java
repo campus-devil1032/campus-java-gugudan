7842731118bda66966efd48ef747e23dd1cd30a3
uxia99 <306406j@gmail.com>
2023-05-03 15:45:57 +0900

    구구단

--- Class6_Eclipse/src/multiplication_table.java
+++
@@ -1,4 +0,0 @@
-
-public class multiplication_table {
-
-}
---
+++ Java_Project/src/ex.java
@@ -0,0 +1,14 @@
+
+public class ex {
+	public static void main(String[] args) {
+//		세로 구구단 출력 
+		
+	for(int n1 = 2; n1 <= 9; n1++) {
+		System.out.println(n1 + "단!");
+		for(int n2 = 1; n2 <= 9; n2++) {
+			System.out.println(n1 + "*" + n2 + "=" + (n1*n2));
+		}
+		System.out.println();
+		}
+	}
+}
