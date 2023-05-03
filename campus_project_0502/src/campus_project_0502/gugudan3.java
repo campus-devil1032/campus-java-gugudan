f5dd8bff61d665566d83a2e1443bd3ab893a9fa8
heahea7 <131749616+chaerimyoo1234@users.noreply.github.com>
2023-05-03 23:54:25 +0900

    rim0503

--- campus_project_0502/src/campus_project_0502/gugudan3.java
+++ campus_project_0502/src/campus_project_0502/gugudan3.java
@@ -1,50 +1,34 @@
 package campus_project_0502;
 
 import java.util.Scanner;
+import java.io.IOException;
 
 public class gugudan3 {
-	public static void main(String[] args) {
+	public void print() {
 		Scanner sc = new Scanner(System.in);
+		int dan = 0;
+		System.out.println("구구단 단수를 입력해주세요 : ");
+		while (true) {
+			try {
+				dan = sc.nextInt();
+				if (dan >= 1 && dan <= 999) {
+					for (int i = 1; i < 10; i++) {
+						System.out.println(dan + "X" + i + "=" + dan * i);
+					}
+					break;
+				} else {
+					System.out.println("입력오류!");
+				}
+			} catch (Exception e) {
+				sc = new Scanner(System.in);
+				System.out.println("1~999 범위의 정수를 입력해주세요.");
 
-		System.out.println("구구단 단수를 입력하세요 = ");
-
-		int dan = sc.nextInt();
-
-		System.out.println(dan + "단");
-
-		if (dan >= 1) {
-			for (int i = 1; i < 10; i++)
-				System.out.println(dan + "X" + i + "=" + dan * i);
-		} else {
-			System.out.println("입력 오류입니다!");
+			}
 		}
-
-	}
-}
-
-class ExceptionEx
-
-{
-
-	public static void main(String args[])
-
-	{
-
-		try {
-
-			if (dan < 0 || dan > 999) {
-				throw new Exception("입력범위 '1 ~ 999'를 벗어났습니다.");
-
-		} catch (Exception e) {
-
-			System.out.println("에러 메시지 : " + e.getMessage());
-
-			e.printStackTrace();
-
-		}
-
-		System.out.println("프로그램이 정상 종료되었음.");
-
 	}
 
+	public static void main(String[] args) throws IOException {
+		gugudan3 in = new gugudan3();
+		in.print();
+	}
 }
