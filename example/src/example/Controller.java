a674df53a2a8cb0bf6191cc84e9d000c2adcf998
Nodept <gupo941020@naver.com>
2023-05-03 15:21:20 +0900

    예외처리 추가한 구구단입니다.

--- example/src/example/Controller.java
+++ example/src/example/Controller.java
@@ -1,22 +1,30 @@
 package example;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Controller {
 
-	public void name() {
-		Scanner sc = new Scanner(System.in);
-		int temp, num1;
-		num1 = sc.nextInt();
-		if ((num1 >= 1) && (num1 <= 999)) {
-			for (int i = 1; i < 10; i++) {
-				temp = num1 * i;
-				System.out.printf("%d * %d = %d\n", num1, i, temp);
+	public void gugudan() {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		int i;
+		try {
+			i = Integer.parseInt(bf.readLine());
+			for (int j = 1; j < 10; j++) {
+				System.out.printf("%d * %d = %d\n", i, j, i * j);
 			}
-		} else {
-			System.out.println("1~999안의 값을 입력하세요");
+		} 
+		catch (IOException e) {
+			System.out.println("정수를 입력하시오...");
+		} 
+		catch (NumberFormatException e) {
+			System.out.println("정수를 입력하시오...");
+		} 
+		catch (Error e) {
+			System.out.println("여기서 오류를 캐치하네요?");
 		}
-		sc.close();
 	}
 
 }
+
--- example/src/example/View.java
+++ example/src/example/View.java
@@ -1,9 +1,9 @@
 package example;
 
-public class Firstclass {
+public class View {
 
 	public static void main(String[] args) {
 		Controller con = new Controller();
-		con.name();
+		con.gugudan();
 	}
 }
