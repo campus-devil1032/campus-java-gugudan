e575cd1735bde88fadfd2aecfbdf88ccd68cf254
hgdd2005 <hgdd2005@gmail.com>
2023-05-03 15:14:23 +0900

    gugudan2

--- campus-backend-project1/src/multiplication.java
+++ campus-backend-project1/src/multiplication.java
@@ -1,41 +1,49 @@
 import java.util.Scanner;
+import java.io.IOException;
 
 public class multiplication {
 
 	public static void main(String[] args) {
-		
 		view();
 		// 구구단을 출력 한다.
 		// 1단부터 9단까지 출력한다.
+	}
 
-		
-}
-static void view() {
-	
-	boolean retry = true;//프로그램 다시 실행을 위한 불
+	static void view() {
+		boolean retry = true;// 프로그램 다시 실행을 위한 불
 		while (retry) {
-			int i;
 			Scanner number = new Scanner(System.in);
 			System.out.println("원하는 구구단의 단을 입력하십시오. ");
-			i = number.nextInt();
-			if (0 < i && i < 999)//0부터 999안의 숫자만 입력
-			{
-				for (int j = 1; j < 10; j++) {
-					System.out.println(i + "*" + j + "=" + j * i); //구구단 출력
+			String num = number.next();
+			if(test(num)) { // 실수 정수판단
+				int i =Integer.parseInt(num);
+				if (0 < i && i < 999)// 0부터 999안의 숫자만 입력
+				{
+					for (int j = 1; j < 10; j++) {
+						System.out.println(num + "*" + j + "=" + j * i); // 구구단 출력
+					}
+				} else {
+					System.out.println("입력하신 숫자가 너무 크거나 음수입니다.");
 				}
-
-			} else {
-				System.out.println("입력하신 숫자가 너무 크거나 음수입니다.");
 			}
-			
-			System.out.println("다시 시작하시겠습까? (y/n)");
+			System.out.println("다시 시작하시겠습까? (종료를 원할시 n을 입력하십시오)");
 			String check = number.next();
-			if (check.equals("n"))//입력 문자가 n이면 retry갑변경
+			if (check.equals("n"))// 입력 문자가 n이면 retry 값 변경
 			{
 				retry = false;
 			}
 		}
 		System.out.println("구구단 프로그램 종료");
 		System.exit(0);
+	}
+
+	public static boolean test(String s) { //실수 정수 판단
+		try {
+			Integer.parseInt(s);
+			return true;
+		} catch (NumberFormatException e) {
+			System.out.println("올바른 정수가 아닙니다.");
+			return false;
+		}
 	}
 }
