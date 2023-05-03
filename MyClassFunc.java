2e99358596f36cf51aaa052702bf0b8321dda4e1
jsj102 <acdw123@naver.com>
2023-05-03 14:45:37 +0900

    add numformat exception

--- MyClassFunc.java
+++ MyClassFunc.java
@@ -5,6 +5,7 @@ public class MyClassFunc {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
+		String scannumber = "";
 		int max = 0;
 		int min = 0;
 
@@ -13,16 +14,29 @@ public class MyClassFunc {
 		// 정수외에 입력을 받았을경우 예외처리는 어떻게 해야할지 모르겠습니다. 구글링도 해보겠습니다.
 		for (; true;) {
 			try {
-
+				
 				System.out.println("출력하실 구구단의 시작 단수를 정수로 입력해주세요.(1~999)");
-				min = sc.nextInt();
+				scannumber = sc.next();
+				if (isInteger(scannumber)!=true) {
+					scannumber = "";
+					continue;
+				}else {
+					min=Integer.parseInt(scannumber);
+					scannumber="";
+				}
 				System.out.println("출력하실 구구단의 끝 단수를 정수로 입력해주세요.(1~999)");
-				max = sc.nextInt();
+				scannumber = sc.next();
+				if (isInteger(scannumber)!=true) {
+					scannumber = "";
+					continue;
+				}else {
+					max=Integer.parseInt(scannumber);
+					scannumber="";
+				}
 
-			} catch (Exception e) {
+			} catch (NumberFormatException e) {
 				System.out.println("입력 받은 값이 정수가 아닙니다..");
 				
-				throw e;
 			}
 
 			if (min != 0) {
@@ -46,6 +60,16 @@ public class MyClassFunc {
 
 		}
 
+	}
+	
+	public static boolean isInteger(String s) {
+		try {
+			Integer.parseInt(s);
+			return true;
+		}catch(NumberFormatException e){
+			return false;
+		}
+		
 	}
 
 }
