6694c9532f73d70b02126df402d5a13d27958684
shj0212 <215212g@gmail.com>
2023-05-04 10:27:12 +0900

    계산기도 구현

--- 2week/MyFirstClass4.java
+++
@@ -1,9 +0,0 @@
-package homework;
-
-public class MyFirstClass4 {
-
-	public static void main(String[] args) throws Exception{	//main
-		Calculator_Controller controller = new Calculator_Controller();
-		controller.inputData();
-	}
-}
