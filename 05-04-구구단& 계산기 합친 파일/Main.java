da011906ac4b75335a0e7a6dff786fc68fba2a20
andle7 <hbshin1809@gmail.com>
2023-05-04 11:32:22 +0900

    구구단&계산기 합친 파일

---
+++ 05-04-구구단& 계산기 합친 파일/Main.java
@@ -0,0 +1,14 @@
+package test;
+
+public class Main {
+	
+	public static void main(String[] args) {
+	
+
+		Dto guguObject = new Dto(9);
+		guguObject.setPrint("¿Ï·á");
+		System.out.println(guguObject.getPrint());
+	
+	}
+	
+}
