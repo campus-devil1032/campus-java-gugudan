151a0b917fa84223bce5ea3819db5ce41c70e32b
Lee Seo Young <88192106+seoyoung1013@users.noreply.github.com>
2023-05-04 11:22:36 +0900

    구구단

---
+++ src/MyFirstClass.java
@@ -0,0 +1,54 @@
+
+import java.util.*;
+
+public class MyFirstClass {
+
+	public static void gugudan(int n) {
+		System.out.println(n + " 단 ");
+		for(int i = 1; i<=9; i++) {
+			System.out.println(n + "*" + i + "=" + (n*i));
+		}
+		System.out.println();
+	}
+	
+	public static void main(String[] args) {
+		//구구단을 출력한다.
+		//1단부터 9단까지 출력한다.
+		// Optinal :유저에게 단 수를 입력받는다.
+		//			이 경우 1~999까지의 입력만 유효하게 예외처리를 한다.
+		// Optional : 비즈니스 로직 분리 해보세요. View 단과 비즈니스 로직 클래스를 따로 생성해서 코드를 메인에서 분리해보세요.
+		
+		// 2단부터 시작하고 9단까지 실행하며 반복문이 한번 돌때마다 1씩 증가 시킵니다.
+		
+		
+		/*for(int i=2; i < 10; i++) {
+			System.out.println(i + "단을 출력 합니다.");
+            
+ 			for(int j=1; j < 10; j++) {
+				System.out.println(i + " x " + j + " = " + i * j);
+			}
+			System.out.println();
+		}*/
+		
+		
+		/*System.out.println("구구단을 실행할 숫자를 입력하세요 : ");
+		Scanner sc = new Scanner(System.in);
+		
+		int dan = sc.nextInt();
+		if(dan >=1 && dan <=999) {
+
+			for (int i = 1; i<10; i++) {
+				System.out.println(dan + " * " + i + " = " + (dan * i));
+				
+			}
+		}
+		
+		*/
+		
+		for(int i =2; i<=9; i++) {
+			gugudan(i);
+		}
+		
+	}
+
+}
---
+++ src/gugudan.java
@@ -0,0 +1,48 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+class GugudanFun {
+
+    int num;
+
+    public GugudanFun(int num) {
+        this.num = num;
+    }
+
+    public void print() {
+        
+    	for (int i = 1; i <= 9; i++) {
+            int result = num * i;
+            System.out.printf("%d X %d = %2d\n", num, i, result);
+        }
+    }
+}
+
+public class gugudan extends GugudanFun {
+    public gugudan(int num) {
+        super(num);
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = 0;
+        System.out.print("원하는 단수의 숫자만 입력하세요 : ");
+
+        while (true) {
+            try {
+                n = sc.nextInt();
+                if (n < 1 || n > 999) {
+                    System.out.print("1와 9사이의 숫자를 입력해주세요 : ");
+                    continue;
+                }
+                break;
+            } catch (InputMismatchException e) {
+                System.out.print("숫자만 입력이 가능합니다. 다시 입력해주세요 : ");
+                sc.nextLine();
+            }
+        }
+        GugudanFun gugudanTable = new GugudanFun(n);
+        gugudanTable.print();
+    }
+}
