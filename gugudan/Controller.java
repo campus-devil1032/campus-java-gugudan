af8dbd8fbea02810ccc7081e5e62ac16d812f9b1
Nodept <gupo941020@naver.com>
2023-05-04 10:55:55 +0900

    계산기 기능까지 추가했습니다. MVC패턴은 잘 이해를 못해서 최대한 구현해보았습니다.

--- gugudan/Controller.java
+++ gugudan/Controller.java
@@ -1,20 +1,56 @@
 package gugudan;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.Arrays;
+import java.io.InputStreamReader;
 
 public class Controller {
-	public static void main(String[] args) throws IOException {
-		//선언부
+	public static void main(String[] args) {
+		// 선언부
 		int num1;
 		int[] num2 = new int[10];
 		Model model = new Model();
 		View view = new View();
-		//입력 출력
-		System.out.println("숫자를 입력하세요!!");
-		num1 = model.enterValue();
-		num2 = model.calculation(num1);
-		view.prettyView(num2, num1);	
+		// 입력 출력
+
+		while (true) {
+			System.out.println("1:계산기	2:구구단	3:종료");
+			BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+			int num3, operation;
+			double num4, num5;
+
+			try {
+				num3 = Integer.parseInt(bf.readLine()); // 1 계산기, 2 구구단 3, 종료
+				if (num3 == 1) {
+					System.out.println("숫자를 입력하세요!!");
+					num4 = model.enterDoubleValue();
+					System.out.println("1:+ 	2:- 	3:* 	4:/ 	5:%");
+					operation = model.enterIntValue();
+					System.out.println("숫자를 입력하세요!!");
+					num5 = model.enterDoubleValue();
+					System.out.println("결과값: " + model.calculator(operation, num4, num5));
+				} else if (num3 == 2) {
+					System.out.println("숫자를 입력하세요!!");
+					num1 = model.enterIntValue();
+					num2 = model.calculation(num1);
+					view.gugudanView(num2, num1);
+				} else if (num3 == 3) {
+					System.out.println("종료....");
+					break;
+				} else {
+					throw new UserException();
+				}
+
+			}catch (NumberFormatException e) {
+				System.out.println("입력오류..");
+			}
+			catch (IOException e) {
+				System.out.println("입력오류..");
+			}catch (RuntimeException e) {
+				System.out.println("다시 작성해주세요");
+			}
+
+		}
 	}
-	
+
 }
--- gugudan/Model.java
+++ gugudan/Model.java
@@ -1,38 +1,68 @@
 package gugudan;
 
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 class Model {
-	class UserException extends RuntimeException {
-		public UserException(String a) {
-			super(a);
-		}
-	}
-	
-	public int enterValue() {
-		int num1 = 0 ;
+
+	// 구구단 값 입력받기
+	public int enterIntValue() throws NumberFormatException, IOException {
+		int num1 = 0;
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-		try {
-			num1 = Integer.parseInt(bf.readLine());
-			if (num1 < 1 || num1 > 10) {
-				throw new UserException("1~9사이만 입력하세요");
-			}
-		} catch (IOException e) {
-			e.getStackTrace();
+		num1 = Integer.parseInt(bf.readLine());
+		if (num1 < 1 || num1 > 9) {
+			throw new UserException();
 		}
 		return num1;
 	}
 
+	// 구구단 배열로 입력받기
 	public int[] calculation(int num1) {
-		int i,j;
+		int i, j;
 		int[] k = new int[9];
 		i = num1;
 		for (j = 1; j < 10; j++) {
-			k[j-1] = i*j;
+			k[j - 1] = i * j;
 		}
 		return k;
 
 	}
+
+	// 계산기 값 입력받기
+	public double enterDoubleValue() throws NumberFormatException, IOException {
+		double num2 = 0;
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		num2 = Double.parseDouble(bf.readLine());
+		return num2;
+	}
+
+	// 계산기
+	public double calculator(int num, double num1, double num2) {
+		double result = 0;
+		switch (num) {
+		case 1:
+			result = num1 + num2;
+			break;
+		case 2:
+			result = num1 - num2;
+			break;
+		case 3:
+			result = num1 * num2;
+			break;
+		case 4:
+			result = num1 / num2;
+			break;
+		case 5:
+			result = num1 % num2;
+			break;
+		}
+
+		return result;
+	}
+}
+
+//사용자정의 예외처리 클래스
+class UserException extends RuntimeException {
+
 }
