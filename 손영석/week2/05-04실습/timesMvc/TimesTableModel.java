d177104f42864c9c17fd9e155e7ae19b9a514120
ReLuee <son7162@gmail.com>
2023-05-05 12:34:31 +0900

    23-05-04 실습

---
+++ 손영석/week2/05-04실습/timesMvc/TimesTableModel.java
@@ -0,0 +1,44 @@
+package timesMvc;
+
+public class TimesTableModel {
+
+	// 구구단
+	public void multiplication(int num) {
+
+		if (num < 1 || num > 999) { // 입력범위제한
+			System.err.println("1에서 999 사이의 정수만 입력하세요.");
+			return;
+		}
+
+		System.out.println(num + "단을 출력합니다.");
+
+		for (int i = 1; i < 10; i++) { // 계산
+			System.out.println(num + " x " + i + " = " + (num * i));
+		}
+
+	}
+
+	// 계산기
+	public void calculate(String operator, double num1, double num2) {
+
+		double result; // 결과값 변수
+
+		if (operator.equals("+")) { // 덧셈
+			result = num1 + num2;
+		} else if (operator.equals("-")) { // 뺄셈
+			result = num1 - num2;
+		} else if (operator.equals("*")) { // 곱셈
+			result = num1 * num2;
+		} else if (operator.equals("/") && num1 != 0 && num2 != 0) { // 나눗셈
+			result = num1 / num2;
+		} else if (operator.equals("%") && num1 != 0 && num2 != 0) { // 나머지 구하기
+			result = num1 % num2;
+		} else { // 입력이 잘못된 경우
+			System.err.println("잘못된 입력입니다.");
+			return;
+		}
+		// 결과값 출력
+		System.out.println(num1 + " " + operator + " " + num2 + " = " + result);
+	}
+
+}
