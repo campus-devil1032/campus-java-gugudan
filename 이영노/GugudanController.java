5d705fd8e2dde31542ac073e2a969a0a547e56b8
2ono <clt3555@gmail.com>
2023-05-09 16:08:57 +0900

    add exception

--- 이영노/GugudanController.java
+++ 이영노/GugudanController.java
@@ -4,36 +4,31 @@ public class GugudanController {
 	private Gugudan model;
 	private GugudanView view;
 	private int GugudanConNum;
-	
+	String stInput;
 
 	public GugudanController(Gugudan model, GugudanView view) {
-		Scanner scan = new Scanner(System.in);
-		System.out.println("구구단 단수를 입력하세요 : ");
-		String stInput = scan.next();
-		this.GugudanConNum = Integer.parseInt(stInput);
+			try {
+			Scanner scan = new Scanner(System.in);
+			System.out.println("구구단 단수를 입력하세요 : ");
+			stInput = scan.next();
+			this.GugudanConNum = Integer.parseInt(stInput);
+			this.model = model;
+			this.view = view;
+			} catch (RuntimeException e){
+			System.out.println(e.getMessage());
+			}
 		
-		this.model = model;
-		this.view = view;
 	}
-
-
-
 
 	public int getGugudanConNum() {
 		return model.getGugudanConNum();
 	}
 
-
-
-
 	public void setGugudanConNum() {
+
 		model.setGugudanConNum(GugudanConNum);
+
 	}
-
-
-
-
-
 
 	public void updateView() {
 		view.createGugudanMVC(model.getGugudanConNum());
--- 이영노/MVCMain.java
+++ 이영노/MVCMain.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+import java.io.IOException;
 
 public class MVCMain {
 
