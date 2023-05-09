46d9dd684abc83c304787b702c4c8bf51365e82d
고영준 <>
2023-05-09 14:58:37 +0900

    koyoungjun gugudan

--- campus-exam-project/src/Gugudan.java
+++ campus-exam-project/src/Gugudan.java
@@ -1,7 +1,24 @@
 package campus_java;
 
 import java.util.Scanner;
-import java.util.InputMismatchException;
+
+class GugudanError extends Exception {
+    GugudanError() {
+
+    }
+    GugudanError(String message) {
+        super(message);
+    }
+}
+
+class Gugu {
+    public void printGugudan(int dan) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(dan + " x " + i + " = " + (dan * i));
+        }
+    }
+}
+
 
 public class Gugudan {
     public static void main(String[] args) {
@@ -12,17 +29,16 @@ public class Gugudan {
                 System.out.print("1부터 999 사이의 정수를 입력하세요 : ");
                 dan = in.nextInt();
                 if (dan < 1 || dan > 999) {
-                    throw new InputMismatchException(); // 예외 발생
+                    throw new GugudanError(); // 예외 발생
                 } else {
                     break;
                 }
-            } catch (InputMismatchException e) {
+            } catch (GugudanError e) {
                 System.out.println("입력에러 : 1 ~ 999 사이의 정수를 입력해주세요");
                 in.nextLine(); // 입력값 받음
             }
         }
-        for (int i = 0; i < 10; i++) { // 9번 반복
-            System.out.println(dan + " x " + i + " = " + (dan * i));
-        }
+        Gugu gugudan = new Gugu();
+        gugudan.printGugudan(dan);
     }
 }
