20d48ec786593bcf18ce14360d0cb000755b2505
oqsis <leaunk@gmail.com>
2023-05-09 17:45:45 +0900

    commitco

--- 최재혁/campus-exam-project/src/gugudanClass.java
+++ 최재혁/campus-exam-project/src/gugudanClass.java
@@ -1,5 +1,5 @@
 
-public class gugudanClass {
+public class GugudanClass {
 
 	public static void main(String[] args) {
 		//구구단을 외자 이중 for문 이용 \t 정렬이용
@@ -14,3 +14,4 @@ public class gugudanClass {
 	}
 
 }
+
