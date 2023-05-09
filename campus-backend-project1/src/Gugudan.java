b221e6beb8471fd7d2cca4e1abd412ad28fe78da
hgdd2005 <hgdd2005@gmail.com>
2023-05-09 14:49:09 +0900

    05 09 class2

--- campus-backend-project1/src/Gugudan.java
+++ campus-backend-project1/src/Gugudan.java
@@ -1,4 +1,3 @@
-
 public class GuGuDan { // 구구단 계산
 	private String number;
 
@@ -8,18 +7,14 @@ public class GuGuDan { // 구구단 계산
 
 	public void setNumber(String number) {
 		this.number = number;
-		
-	 }
-
-	public void test(String number) {
-		int i = Integer.parseInt(number);								//입력한  String을 int로 변환
-		if (0 < i && i <= 999)											// 0부터 999안의 숫자만 입력
+		int num = Integer.parseInt(number); // 입력한 String을 int로 변환
+		if (0 < num && num <= 999) // 0부터 999안의 숫자만 입력
 		{
-			for (int j = 1; j < 10; j++) {
-				System.out.println("	" + i + "*" + j + "=" +(i*j)); 	// 구구단 출력
+			for (int dan = 1; dan < 10; dan++) {
+				System.out.println("	" + num + "*" + dan + "=" + (num * dan)); // 구구단 출력
 			}
 		} else {
-			System.out.println("입력하신 숫자가 너무 크거나 음수입니다.");			//숫자가 범위 외 일때 출력
+			System.out.println("입력하신 숫자가 너무 크거나 음수입니다."); // 숫자가 범위 외 일때 출력
 		}
 	}
 }
--- campus-backend-project1/src/Main.java
+++ campus-backend-project1/src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		View View= new View();
-		View.Screen();								//화면 출력
+		View.Screen(); // 입력 및 출력화면
 	}
 
 }
